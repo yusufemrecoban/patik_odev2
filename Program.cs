630100f02bb6f3a359251b032055a27cda821013
yusuf <yusuffemreecobann@gmail.com>
2023-07-29 01:17:17 +0300

    patika ödev 2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace patik_odev{
+    class Program{
+        static void Main(string[] args){
+              Console.WriteLine("kaç tane sayı girmek istiyorsunuz: ");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              int[] sayilar = new int[sayi];
+              Console.WriteLine("kaça bölmek istiyorsunuz : ");
+              int kontrol = Convert.ToInt32(Console.ReadLine());
+                for(int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine("sayı giriniz: ");
+                    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                foreach( int sayim in sayilar)
+                {
+                    if( sayim%kontrol==0)
+                    {
+                        Console.WriteLine("tam bölünenler:");
+                        Console.WriteLine(sayim);
+                    }
+                }
+           Console.ReadLine();
+        }
+    }
+}
